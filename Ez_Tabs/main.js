5bc6f99ffa895c74d057b9ac1a1bb9fd429aaa13
woojin624 <54699702+woojin624@users.noreply.github.com>
2020-03-17 21:20:44 +0900

    tabs finish

--- Ez_Tabs/main.js
+++ Ez_Tabs/main.js
@@ -9,8 +9,23 @@ document.getElementById(tabs-1).style.display='block';
 */
 
 var targetLink = document.querySelectorAll(".tab-menu a");
-console.log(targetLink);
+var tabContent = document.querySelectorAll("#tab-content > div");
 
-var orgTarget = "#tabs-1"; // a.replace('b','c');
-var tabTarget = orgTarget.replace("#", "");
-document.getElementById(tabTarget).style.display = "block";
+for (var i = 0; i < targetLink.length; i++) {
+  targetLink[i].addEventListener("click", function(e) {
+    e.preventDefault();
+    var orgTarget = e.target.getAttribute("href");
+    var tabTarget = orgTarget.replace("#", "");
+    for (var x = 0; x < tabContent.length; x++) {
+      tabContent[x].style.display = "none";
+    }
+    document.getElementById(tabTarget).style.display = "block";
+
+    for (var j = 0; j < targetLink.length; j++) {
+      targetLink[j].classList.remove("active");
+      e.target.classList.add("active");
+    }
+  });
+}
+
+document.getElementById("tabs-1").style.display = "block";
