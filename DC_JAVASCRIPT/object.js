09ae4acd55eb254aed102bacb3dc627857331dd3
woojin624 <54699702+woojin624@users.noreply.github.com>
2020-05-14 02:12:52 +0900

    Update object.js

--- DC_JAVASCRIPT/object.js
+++ DC_JAVASCRIPT/object.js
@@ -99,7 +99,9 @@ console.log(user4);
 
 // another example
 const fruit1 = { color: "red" };
+
 const fruit2 = { color: "blue", size: "big" };
+
 const mixed = Object.assign({}, fruit1, fruit2);
 console.log(mixed.color);
 console.log(mixed.size);
